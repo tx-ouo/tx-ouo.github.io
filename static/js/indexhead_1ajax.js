df1251bd191e064711b43303a97943155adbfe26
tx-ouo <m13104742653@163.com>
2022-09-27 15:36:49 +0800

    0.21.0.3：小修.

--- static/js/indexhead_1ajax.js
+++ static/js/indexhead_1ajax.js
@@ -34,13 +34,72 @@ $(function(){
             var obj =JSON.parse(c)
             var po=$("<li></li>");
             obj.forEach(i => {
-                var li="<li>"+i.userId+","+i.userName+","+i.userNum+","+i.userAddress+","+i.userstuts+"</li>";
+                var li="<li>"+"编号："+i.userId+"，"+"施工队名："+i.userName+"，"+"人数："+i.userNum+"，"+"施工地："+i.userAddress+"，"+"状态："+i.userstuts+"</li>";
                 po.append(li);
             });
             $(".sj").append(po);
         });
     });
+
+    //清空按钮
+    $(".btn2").click(function(){  
+        //清空所有
+       // $(".sj").html('');
+
+       $(".sj li").remove();
+       var len =$(".sj li").length;
+       if (len==0) {
+        alert("已完成");
+       } else {
+        alert("程序出错");
+       }  
+    })
+});
+
+$(function(){
+    const v = new Vue({
+        el:'#root',
+        data(){
+           return{
+            name:'鲸鲨队',
+            address:"河南",
+            url:"../index.html",
+            nowTime:"",
+          }
+        },     
+        methods:{
+            getTime(){
+                let year = new Date().getFullYear();
+                let month = new Date().getMonth()+1;
+                let day = new Date().getDate();
+                let hour = new Date().getHours();
+                let minute = new Date().getMinutes();
+                let second = new Date().getSeconds();
+                this.nowTime = year+"-"+month+"-"+day+"-"+hour+":"+minute+":"+second;    
+            },           
+        },
+        //在mounted钩子函数中调用nowTimes函数 
+        mounted(){
+            this.getTime()
+        },
+  
+     
+    });
+    // v.$mount('#root');
+
+    // let 变量={属性：值，，}
+    // Object.defineProperty(变量,新添的属性名,新添{
+    //     value:,
+    //     enumerable:true,控制属性是否可以枚举
+    //     writable:true,是否可以被修改
+    //     configurable:true,被删除
+    // get()
+    // set()
+    // })
+    
+  
 })
+
 
 
 
